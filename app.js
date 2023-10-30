43befd19184430facfa26ab631ce3b606e28ec2b
kly-arkhabmaulana <63295377+kly-arkhabmaulana@users.noreply.github.com>
2023-10-30 10:49:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const io = socketIO(server);
 
 const onlineUsers = new Set(); // Daftar pengguna online
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 
 io.on('connection', (socket) => {
   onlineUsers.add(socket.id); // Menambahkan pengguna ke daftar pengguna online
@@ -23,8 +17,6 @@ io.on('connection', (socket) => {
     io.emit('online-users', onlineUsers.size);
   });
 });
-
-app.use(express.static(__dirname));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
